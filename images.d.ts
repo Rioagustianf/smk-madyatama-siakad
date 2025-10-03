ef26dcfbfa9507d3600d8c13eb4af4fa32634257
Rio Agustian <rioagustian2001@gmail.com>
2025-10-04 01:30:02 +0700

    chore: Tambahkan dukungan untuk file definisi TypeScript dalam tsconfig.json

---
+++ images.d.ts
@@ -0,0 +1,24 @@
+declare module "*.png" {
+  const src: import("next/dist/shared/lib/get-img-props").StaticImport;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: import("next/dist/shared/lib/get-img-props").StaticImport;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: import("next/dist/shared/lib/get-img-props").StaticImport;
+  export default src;
+}
+
+declare module "*.JPG" {
+  const src: import("next/dist/shared/lib/get-img-props").StaticImport;
+  export default src;
+}
+
+declare module "*.PNG" {
+  const src: import("next/dist/shared/lib/get-img-props").StaticImport;
+  export default src;
+}
