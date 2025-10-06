f39b55586c3caabe0d370f6e8761d458a91e317e
Rio Agustian <rioagustian2001@gmail.com>
2025-10-06 20:29:24 +0700

    refactor: Update Spinner component dengan mengubah icon

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,16 +1,21 @@
-import { Loader2Icon } from "lucide-react"
+import * as React from "react";
+import { Loader2 } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+type SpinnerProps = Omit<React.SVGProps<SVGSVGElement>, "ref">;
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
-  return (
-    <Loader2Icon
+const Spinner = React.forwardRef<SVGSVGElement, SpinnerProps>(
+  ({ className, ...props }, ref) => (
+    <Loader2
+      ref={ref}
       role="status"
       aria-label="Loading"
-      className={cn("size-4 animate-spin", className)}
+      className={cn("h-4 w-4 animate-spin", className)}
       {...props}
     />
   )
-}
+);
 
-export { Spinner }
+Spinner.displayName = "Spinner";
+
+export { Spinner };
